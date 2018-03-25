94b326af8a2add551c8d8eca9ec86535dfffea33
Dimmi <pechunka11@gmail.com>
2018-03-25 17:00:29 +0200

    error commit

---
+++ client/src/site/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Header from './Header'
+
+class Header extends Component {
+  render() {
+    return(
+      <header className="header">
+        Header
+      </header>
+    )
+  }
+}
+
+export default Header;
