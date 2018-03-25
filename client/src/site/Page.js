4bf438687a00030f35d6568d8a626e57527b33d4
Dimmi <pechunka11@gmail.com>
2018-03-26 01:09:34 +0200

    creating Home page

--- client/src/site/Page.js
+++ client/src/site/Page.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {Route} from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+
 import Home from './Home'
+import Header from './Header'
 
-import './styles/page.sass'
+import './styles/page.css'
 
-const Page = ({match}) => {
+class Page extends Component {
 
-  return(
-      <div className="container">
-        <Route exact path={`${match.url}/`} component={Home} />
-      </div>
-  )
+  componentDidMount = () => {
+    this.props.getMenu()
+  }
+
+  render() {
+    return(
+        <div className="container Sites">
+          <Header />
+          <Route exact path={`${this.props.match.url}/`} component={Home} />
+        </div>
+    )
+  }
+
 }
 
-export default Page;
+export default connect(null, actions)(Page);
